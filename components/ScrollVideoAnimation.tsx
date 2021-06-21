3569381ce21ce9b3f103e43a2d679154800e5b7a
Ninjo <ninjomcs@gmail.com>
2021-06-21 14:20:31 +0200

    feat: smoother and more optimized scroll bound animations

--- components/ScrollVideoAnimation.tsx
+++ components/ScrollVideoAnimation.tsx
@@ -1,39 +1,43 @@
-import {FC, useRef, useEffect, MutableRefObject} from "react";
+import { FC, MutableRefObject, useEffect, useRef } from "react";
 
 type ScrollVideoAnimationProps = { parentRef: MutableRefObject<HTMLElement> };
 
 export const ScrollVideoAnimation: FC<ScrollVideoAnimationProps> = ({ parentRef }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const frameCount = 47;
+    let imageArray;
 
-    const preloadImages = () => {
-        for (let i = 0; i < frameCount; i++) {
+    const loadImages = (): HTMLImageElement[] => {
+        let images = [];
+        for (let i = 1; i <= frameCount; i++) {
             const img = new Image();
             img.src = currentFrame(i);
+            images.push(img);
         }
+        return images;
     };
+
+    useEffect(() => {
+        imageArray = loadImages();
+    }, [])
 
     const currentFrame = (index: number): string => (
         `/png/anim/test_anim${index.toString().padStart(5, '0')}.png`
     );
 
-    const updateImage = (index: number, img: HTMLImageElement): void => {
+    const updateImage = (index: number): void => {
         const context = canvasRef.current.getContext("2d");
-        img.src = currentFrame(index);
-        context.drawImage(img, 0, 0);
+        context.drawImage(imageArray[index], 0, 0);
     }
 
-    useEffect(() => preloadImages);
-
     useEffect(() => {
-        const img = new Image();
         window.addEventListener("scroll", () => {
             const scrollTop = window.scrollY;
             const maxScrollTop = parentRef.current.scrollHeight - window.innerHeight;
             const scrollFraction = scrollTop / maxScrollTop;
             const frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount));
             console.log(frameIndex);
-            requestAnimationFrame(() => updateImage(frameIndex + 1, img));
+            requestAnimationFrame(() => updateImage(frameIndex));
         })
     }, []);
 
