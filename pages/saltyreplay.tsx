6a9b377673518cbb62243312a6f6c00cdc45f868
Natalie Lenart <ninjomcs@gmail.com>
2021-07-01 13:01:30 +0200

    fix: saltyreplay responsiveness

--- pages/saltyreplay.tsx
+++ pages/saltyreplay.tsx
@@ -2,16 +2,18 @@ import { FC } from "react";
 import Image from "next/image";
 
 const SaltyReplay: FC = () => (
-    <section className="w-full h-screen bg-gray-800 flex flex-col lg:flex-row lg:justify-around justify-center gap-y-12 items-center text-white">
-        <div className="font-raleway text-7xl md:text-7.5xl min-w-max mx-10 text-center lg:text-left">
+    <section className="w-full h-screen bg-gray-800 flex flex-col lg:flex-row lg:justify-around justify-center gap-y-8 items-center text-white">
+        <div className="font-raleway text-7xl lg:text-7.5xl min-w-max mx-10 text-center lg:text-left">
             <h1>replays.</h1>
-            <h1 className="text-frenchviolet"><span className="text-amethyst">red</span>
-                <span className="text-celeste">ef</span>ined.</h1>
+            <h1 className="text-frenchviolet">
+                <span className="text-amethyst">red</span>
+                <span className="text-celeste">ef</span>ined.
+            </h1>
         </div>
-        <div className="mx-10 max-w-lg lg:max-w-full">
+        <div className="mx-10 max-w-md lg:max-w-full">
             <Image src="/png/saltyreplay.png" width={900} height={554.07} />
         </div>
-        <div className="absolute bottom-0 text-gray-500 my-10">
+        <div className="absolute bottom-0 text-gray-500 my-8">
             <h4>Coming sometime between now and the future</h4>
         </div>
     </section>
